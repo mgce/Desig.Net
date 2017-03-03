a914df5e553ae0dcdf487c06ad293da2c39fe419
Contend <blogcontend@gmail.com>
2017-03-03 17:33:47 +0100

    Added Nhibernate and Models

---
+++ Designet/Designet/Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Designet.Models
+{
+    public class Note
+    {
+        public virtual int Id { get; set; }
+        public virtual string Description { get; set; }
+        public virtual DateTime Created { get; set; }
+
+        public virtual int OrderId { get; set; }
+        public virtual Order Order { get; set; }
+    }
+}
---
+++ Designet/Designet/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Designet.Models
+{
+    public class Order
+    {
+        public virtual int Id { get; set; }
+        public virtual string Description { get; set; }
+        public virtual decimal Price { get; set; }
+        public virtual DateTime Created { get; set; }
+        public virtual DateTime Deadline { get; set; }
+
+        public virtual int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
