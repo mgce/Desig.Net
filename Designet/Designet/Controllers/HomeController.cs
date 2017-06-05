de021d14099c3488a559ca225b96e82e445eccc9
contend <blogcontend@gmail.com>
2017-06-05 21:02:22 +0200

    test

--- Designet/Designet/Controllers/HomeController.cs
+++ Designet/Designet/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Designet.Controllers
 {
@@ -10,7 +6,7 @@ namespace Designet.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Home Page Test";
 
             return View();
         }
